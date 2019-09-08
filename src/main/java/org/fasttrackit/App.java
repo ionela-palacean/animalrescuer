4cf09b5b164a6f0ed38cb8be4172181fed79f166
Ionela Palacean <ione.caia85@gmail.com>
2019-09-08 20:21:44 +0300

    reducerea nivelului de foame cu hranire succesiva :) testare for

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,6 +1,5 @@
 package org.fasttrackit;
 
-import java.sql.SQLOutput;
 
 public class App
 {
@@ -12,18 +11,19 @@ public class App
                 pet.name = "Carlitos";
                 pet.age=25;
                 pet.healtCondition=10;
-                pet.favoriteFood="minced meat";
+                pet.favoriteFood="Purina";
                 pet.favoriteActivity="walk";
                 pet.hungryLevel=5;
                 pet.mood=3;
-Pet pet2= new Pet();
-pet.name="Dixon";
-pet.age=6;
-pet.healtCondition=8;
-pet.favoriteFood=" any meat";
-pet.favoriteActivity="walk";
-pet.hungryLevel=1;
-pet.mood=3;
+
+// Pet pet2= new Pet();
+// pet.name="Dixon";
+// pet.age=6;
+// pet.healtCondition=8;
+// pet.favoriteFood=" any meat";
+// pet.favoriteActivity="walk";
+// pet.hungryLevel=1;
+// pet.mood=3;
 
                 Rescuer rescuer=new Rescuer();
                 rescuer.name="Andrei";
@@ -35,12 +35,12 @@ pet.mood=3;
 
                 pet.rescuer=rescuer;
                 pet.veterinary=veterinary;
-pet2=pet; //suprascriere;
-                System.out.println(pet.name);
-                System.out.println(pet.age);
-                System.out.println(pet.rescuer);
-                System.out.println(pet.veterinary);
+//pet2=pet; //suprascriere;
+               System.out.println(pet.name);
+               System.out.println(pet.hungryLevel);
+               // System.out.println(pet.rescuer);
+               // System.out.println(pet.veterinary);
+                pet.getHungryLevel(5);
 
-
-}
+            }
         }
--- src/main/java/org/fasttrackit/Pet.java
+++ src/main/java/org/fasttrackit/Pet.java
@@ -5,7 +5,7 @@ public class Pet {
     String name;
     Integer age;
     Integer healtCondition;
-    Integer hungryLevel;
+    double hungryLevel;
     String favoriteFood;
     String favoriteActivity;
     Integer mood;
@@ -13,7 +13,26 @@ public class Pet {
     Rescuer rescuer;
     Veterinarian veterinary;
 
+    public double getHungryLevel(double hungryLevel) {
+
+        System.out.println(name + " will eat the favorite food: " + favoriteFood + " and the hungry level will decrease");
+        hungryLevel--;
+        double newHungryLevel = hungryLevel;
 
 
+        System.out.println("New hungrylevel is:" + newHungryLevel);
 
+        System.out.println("If " + name + " will eat more food the hunggry level will decrease  to 0");
+        for (int  i=0; newHungryLevel>0;i++) {
+            if (newHungryLevel > 0) ;
+            hungryLevel--;
+
+            newHungryLevel = hungryLevel;
+
+            System.out.println(newHungryLevel);
+
+        }
+    return newHungryLevel;
+    }
 }
+
